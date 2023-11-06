1dcfa1577c8031666c1e1afeeb9786c6d9891709
Balint Juhasz <130932233+balint1026@users.noreply.github.com>
2023-11-06 13:45:31 +0100

    add consolelogger and errorlogger

---
+++ src/main/java/com/codecool/ehotel/service/logger/ConsoleLogger.java
@@ -0,0 +1,13 @@
+package com.codecool.ehotel.service.logger;
+
+public class ConsoleLogger implements Logger{
+    @Override
+    public void infoLogger(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public void errorLogger(String message) {
+        System.out.println(message);
+    }
+}
