6b8ba4edcb475ee8448a546a875a439d0c3e7e76
Balint Juhasz <130932233+balint1026@users.noreply.github.com>
2023-11-09 09:26:38 +0100

    now we add a new tab to our sheet whenever we run a simulation

--- src/main/java/com/codecool/ehotel/service/UI/Input.java
+++ src/main/java/com/codecool/ehotel/service/UI/Input.java
@@ -6,7 +6,12 @@ public class Input {
 
     public int getSeasonLength(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How long this season will be?");
+        System.out.println("How long will this season be?");
+        return scanner.nextInt()+1;
+    }
+    public int getGuestAmount(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many guests will there be?");
         return scanner.nextInt()+1;
     }
 }
